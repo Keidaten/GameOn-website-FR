e7117bb17b0c4a537171836463ea2384ae55a029
Jey <jeremyalder@hotmail.fr>
2021-03-09 17:22:53 +0100

    minor fixes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,39 +28,22 @@ function closeModal() {
     modalbg.style.display = "none";
 }
 
-//Form elements
-const formData = document.querySelectorAll(".formData");
-const firstName = document.querySelector("#first");
-const lastName = document.querySelector("#last");
-const eMail = document.querySelector("#email");
-const birthDate = document.querySelector("#birthdate");
-const quantityTourney = document.querySelector("#quantity");
-const formConfirmation = document.querySelector("#form-confirmation");
-
-//Error messages element
-const errorMsgFirstName = document.querySelector("#errorMsgFirstName");
-const errorMsgLastName = document.querySelector("#errorMsgLastName");
-const errorMsgMail = document.querySelector("#errorMsgMail");
-const errorMsgTourney = document.querySelector("#errorMsgTourney");
-const errorMsgCity = document.querySelector("#errorMsgCity");
-const errorMsgCGU = document.querySelector("#errorMsgCGU");
-
-//create user feedbacks
+//create text feedbacks for users // errorElement to target div error element, errorInnerText to chose error message to apply
 function feedbackMessage(errorElement, errorInnerText) {
     errorElement.innerText = errorInnerText;
     errorElement.style.fontSize = "65%";
     errorElement.style.color = "red";
 }
 
+//create visual feedbacks for user // formDataElement to target form input, borderColor to chose border color to apply
 function feedbackVisual(formDataElement, borderColor) {
     formDataElement.focus();
     formDataElement.style.borderColor = borderColor;
 }
 
-//Confirmation messages elements
+//Create feedback for user when form submitted
 const header = document.getElementById("myTopnav");
 
-//Create user feedbacks
 function feedbackFormValid() {
     let feedbackSpan = document.createElement("span");
     feedbackSpan.innerText = "Merci ! Votre réservation a été reçue.";
@@ -72,7 +55,10 @@ function feedbackFormValid() {
     header.insertAdjacentElement("afterend", feedbackSpan);
 }
 
-// Check if inputs are correct
+//                            //
+// Check if inputs are valid  //
+//                            //
+
 let firstNameIsValid = false;
 let lastNameIsValid = false;
 let emailIsValid = false;
@@ -80,7 +66,9 @@ let tourneyIsValid = false;
 let cityIsValid = false;
 let cguIsValid = false;
 
-//Check first name
+//Check if first name is valid
+const firstName = document.querySelector("#first");
+const errorMsgFirstName = document.querySelector("#errorMsgFirstName");
 
 function verifyFirstName() {
     if (!firstName.value.match(/^[a-z ,'-]+$/i) || firstName.value.length < 2 || firstName.value == " " || firstName.value == null) {
@@ -88,48 +76,57 @@ function verifyFirstName() {
         feedbackVisual(firstName, "red");
     } else {
         feedbackMessage(errorMsgFirstName, null);
-        feedbackVisual(firstName, null);
+        feedbackVisual(firstName, "green");
         firstNameIsValid = true;
     }
 }
 
-//Check last name
+//Check if last name is valid
+const lastName = document.querySelector("#last");
+const errorMsgLastName = document.querySelector("#errorMsgLastName");
+
 function verifyLastName() {
     if (!lastName.value.match(/^[a-z ,.'-]+$/i) || lastName.value.length < 2 || lastName.value == " " || lastName.value == null) {
         feedbackMessage(errorMsgLastName, "Veuillez vérifier votre nom de famille");
         feedbackVisual(lastName, "red");
     } else {
         feedbackMessage(errorMsgLastName, null);
-        feedbackVisual(lastName, null);
+        feedbackVisual(lastName, "green");
         lastNameIsValid = true;
     }
 }
 
-//Check email
+//Check if email is valid
+const eMail = document.querySelector("#email");
+const errorMsgMail = document.querySelector("#errorMsgMail");
+
 function verifyMail() {
     if (!eMail.value.match(/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]{3,}[.][a-zA-Z]{2,3}/) || eMail.value == " " || eMail.value == null) {
         feedbackMessage(errorMsgMail, "Veuillez vérifier votre email");
         feedbackVisual(eMail, "red");
     } else {
         feedbackMessage(errorMsgMail, null);
-        feedbackVisual(eMail, null);
+        feedbackVisual(eMail, "green");
         emailIsValid = true;
     }
 }
 
-//Check tourney
+//Check if tourney is valid
+const quantityTourney = document.querySelector("#quantity");
+const errorMsgTourney = document.querySelector("#errorMsgTourney");
+
 function verifyTourney() {
     if (!quantityTourney.value.match(/^[0-9]+$/) || quantityTourney.value == " " || quantityTourney.value == null) {
         feedbackMessage(errorMsgTourney, "Veuillez saisir une valeur numérique");
         feedbackVisual(quantityTourney, "red");
     } else {
         feedbackMessage(errorMsgTourney, null);
-        feedbackVisual(quantityTourney, null);
+        feedbackVisual(quantityTourney, "green");
         tourneyIsValid = true;
     }
 }
 
-//Check city
+//Check if one city is checked
 const location1 = document.getElementById("location1");
 const location2 = document.getElementById("location2");
 const location3 = document.getElementById("location3");
@@ -137,6 +134,7 @@ const location4 = document.getElementById("location4");
 const location5 = document.getElementById("location5");
 const location6 = document.getElementById("location6");
 const checkbox = document.querySelectorAll(".checkbox-input");
+const errorMsgCity = document.querySelector("#errorMsgCity");
 
 function verifyCity() {
     if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
@@ -147,8 +145,9 @@ function verifyCity() {
     }
 }
 
-//Check CGU
+//Check if CGU is checked
 const checkbox1 = document.getElementById("checkbox1");
+const errorMsgCGU = document.querySelector("#errorMsgCGU");
 
 function verifyCGU() {
     if (!checkbox1.checked) {
@@ -159,8 +158,7 @@ function verifyCGU() {
     }
 }
 
-// Validate form
-
+// Form validation
 function validate() {
     //call verifications
     verifyFirstName();
@@ -170,7 +168,7 @@ function validate() {
     verifyCity();
     verifyCGU();
 
-    //Listening input for realtime feedback
+    //Listening inputs for real time feedbacks
     firstName.addEventListener("input", verifyFirstName);
     lastName.addEventListener("input", verifyLastName);
     eMail.addEventListener("input", verifyMail);
@@ -178,7 +176,7 @@ function validate() {
     checkbox1.addEventListener("input", verifyCGU);
     checkbox.forEach((box) => box.addEventListener("input", verifyCity));
 
-    //Form validation
+    //Check if all inputs are valids
     if (firstNameIsValid == true && lastNameIsValid == true && emailIsValid == true && tourneyIsValid == true && cityIsValid == true && cguIsValid == true) {
         return true;
     }
@@ -188,6 +186,6 @@ function validate() {
 }
 
 //Display feedback if form submitted
-if (window.location.href.indexOf("?first=") > 1) {
+if (window.location.href.indexOf("?first=") > -1) {
     feedbackFormValid();
 }
